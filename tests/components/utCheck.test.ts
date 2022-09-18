ff90f7a57df3a60b2daa5c0d47545445440eb8eb
Tom-Shumi <tshumiya281@gmail.com>
2022-09-18 15:33:13 +0900

    add test code

---
+++ tests/components/utCheck.test.ts
@@ -0,0 +1,13 @@
+import { Test } from "components/utCheck";
+
+describe("utCheck class のテスト", () => {
+    const testClass = new Test("エンジニア", 82);
+
+    test("生成されたインスタンスは Test クラスのものである", () => {
+        expect(testClass).toBeInstanceOf(Test);
+    });
+
+    test("年齢は 82 歳です", () => {
+        expect(testClass.age).toBe(82);
+    });
+});
