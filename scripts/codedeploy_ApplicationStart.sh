4ae063941fe696a0bb158b9684fff0ad5b3a06d6
Tom-Shumi <tshumiya281@gmail.com>
2022-09-19 11:35:39 +0900

    fix codedeploy scripts

--- scripts/codedeploy_ApplicationStart.sh
+++ scripts/codedeploy_ApplicationStart.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sudo cd /home/ec2-user/japanese-comics-notice-admin
-sudo docker-compose up -f /home/ec2-user/japanese-comics-notice-admin/docker-compose.yml -d
+sudo docker-compose -f /home/ec2-user/japanese-comics-notice-admin/docker-compose.yml up
--- scripts/codedeploy_ApplicationStop.sh
+++ scripts/codedeploy_ApplicationStop.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sudo cd /home/ec2-user/japanese-comics-notice-admin
-sudo docker-compose down -f /home/ec2-user/japanese-comics-notice-admin/docker-compose.yml
+sudo docker-compose -f /home/ec2-user/japanese-comics-notice-admin/docker-compose.yml down
