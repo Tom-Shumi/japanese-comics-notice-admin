28d8c9a9552b6e4aeda45570b93f8deff0eba5bc
Tom-Shumi <tshumiya281@gmail.com>
2022-09-19 11:49:25 +0900

    test codedeploy scripts

--- scripts/codedeploy_ApplicationStart.sh
+++ scripts/codedeploy_ApplicationStart.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-cat scripts/codedeploy_ApplicationStop.sh
 sudo docker-compose -f /home/ec2-user/japanese-comics-notice-admin/docker-compose.yml up -d
--- scripts/codedeploy_ApplicationStop.sh
+++ scripts/codedeploy_ApplicationStop.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+cat scripts/codedeploy_ApplicationStop.sh
+echo | cat codedeploy_ApplicationStop.sh 
 sudo docker-compose -f /home/ec2-user/japanese-comics-notice-admin/docker-compose.yml down
