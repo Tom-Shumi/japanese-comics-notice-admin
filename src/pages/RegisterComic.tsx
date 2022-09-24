51086a7e5fb46ad2cbdd94bb3d159ed6b0a7f4fc
Tom-Shumi <tshumiya281@gmail.com>
2022-09-24 16:49:05 +0900

    add asin check

--- src/pages/RegisterComic.tsx
+++ src/pages/RegisterComic.tsx
@@ -11,14 +11,6 @@ const RegisterComic: NextPage = () => {
     const [volumeNum, setVolumeNum] = useState("");
     const [releaseDate, setReleaseDate] = useState("");
     const [asin, setAsin] = useState("");
-
-    const doClear = () => {
-        setUsUrl("");
-        setUsTitle("");
-        setVolumeNum("");
-        setReleaseDate("");
-        setAsin("");
-    }
     
     const doRegister = async () => {
 
@@ -48,13 +40,44 @@ const RegisterComic: NextPage = () => {
         setAsin("");
     }
 
+    const checkAsin = async () => {
+        if (asin == "") {
+            return;
+        }
+
+        const res = await axios.get("/api/checkAsin"
+            , {params: {
+                asin: asin
+            }}
+        );
+
+        const result = res.data.result;
+        alert(result);
+        
+        if (result == "NG") {
+            setUsUrl("");
+            setUsTitle("");
+            setVolumeNum("");
+            setReleaseDate("");
+            setAsin("");    
+        }
+    }
+
     return (
         <Layout color='white'>
             <h2>◆Register Comic</h2>
             <div className={styles.container}>
-                <button type="button" className={styles.registerButton} onClick={doClear}>
-                    Clear
-                </button><br />
+                <div className="row">
+                    <h3>ASIN</h3>
+                    <div>
+                        <input type="text" className={styles.asinText} value={asin} onChange={(event) => setAsin(event.target.value)}></input>
+                        <button type="button" className={styles.checkAsinButton} onClick={checkAsin}>Check</button>
+                    </div>
+                </div><br />
+                <div className="row">
+                    <h3>Us Url</h3>
+                    <input type="text" value={usUrl} onChange={(event) => setUsUrl(event.target.value)}></input>
+                </div><br />
                 <div className="row">
                     <h3>Us Title</h3>
                     <input type="text" value={usTitle} onChange={(event) => setUsTitle(event.target.value)}></input>
@@ -66,14 +89,6 @@ const RegisterComic: NextPage = () => {
                 <div className="row">
                     <h3>Release Date</h3>
                     <input type="text" value={releaseDate} onChange={(event) => setReleaseDate(event.target.value)}></input>
-                </div><br />
-                <div className="row">
-                    <h3>Us Url</h3>
-                    <input type="text" value={usUrl} onChange={(event) => setUsUrl(event.target.value)}></input>
-                </div><br />
-                <div className="row">
-                    <h3>ASIN</h3>
-                    <input type="text" value={asin} onChange={(event) => setAsin(event.target.value)}></input>
                 </div><br />
                 <button type="button" className={styles.registerButton} onClick={doRegister}>
                     Register
